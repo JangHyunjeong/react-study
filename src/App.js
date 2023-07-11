784158f5bcaebadcb2fdd45525bf63907c027e05
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-07-11 15:21:10 +0900

    state변경하는 법 : 1. onClick={함수}, 2. state변경함수(새로운state)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useState } from "react";
 import "./App.css";
 
@@ -5,29 +6,37 @@ function App() {
   // 자료 잠깐 저장할땐 변수
   let post = "강남 우동 맛집";
 
-  //#1. state 사용법
-  // 1. import {useState } from 'react'
-  // 2. useState(보관할자료)
-  // 3. let [작명, 작]
-
   let [title, setTitle] = useState([
     "남자 코트 추천",
     "오늘 날씨 어때?",
     "충격 연돈 근황",
   ]);
-  // 왜 state 씀?
-  // 일반 변수는 갑자기 변경되면 html 에 자동으로 반영 안됨
-  // state는 자동 재랜더링됨
-  // 변동 시 자동으로 html에 반영되게 만들고 싶으면 state 쓰셈
+  let [좋아요, 좋아요변경] = useState(0);
 
-  // 자주 변경될거 같은 html state로 만들기
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{title[0]}</h4>
+        <h4>
+          {title[0]}
+          {/* 1. onClick 쓰는법
+          onClick={} 안엔 함수 이름을 넣어야함 
+          onClick={함수명}
+          onClick={()=>{console.log(1)}} 
+          이런식으로 써도 됌 */}
+
+          {/* 2. state 변경하는법
+          항상 state 변경함수 사용
+          state변경함수(새로운state) 
+          - state는 등호로 변경금지
+          - 좋아요변경 이용 > 그래야 재랜더링 잘 됨
+          */}
+          <button type="button" onClick={() => 좋아요변경(좋아요 + 1)}>
+            ❤️ {좋아요}
+          </button>
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
