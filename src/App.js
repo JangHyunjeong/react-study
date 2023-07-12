06556522cc385453dc92546c4735ef91296f6e5e
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-07-12 16:22:50 +0900

    글 발행, 삭제 기능

--- src/App.js
+++ src/App.js
@@ -10,14 +10,25 @@ function App() {
   ]);
   let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-  [1, 2, 3].map(function (a) {
-    return "1213";
-  });
+
   let [currentIdx, setCurrentIdx] = useState(null);
+  let [inputValue, setInputValue] = useState("");
 
   function chnageList() {
     let titleCopy = [...title];
     titleCopy[0] = "여자 코트 추천";
+    setTitle(titleCopy);
+  }
+
+  function post() {
+    let titleCopy = [...title];
+    titleCopy.unshift(inputValue);
+    setTitle(titleCopy);
+  }
+
+  function deleteTitle(idx) {
+    let titleCopy = [...title];
+    titleCopy.splice(idx, 1);
     setTitle(titleCopy);
   }
 
@@ -27,22 +38,21 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      {/* a: 현재 도는 item , i : 0부터  ++ 되는 정수 */}
       {title.map(function (a, i) {
         return (
           <div className="list" key={i}>
-            <h4>
-              <span
-                onClick={() => {
-                  setCurrentIdx(i);
-                  setModal(true);
-                }}
-              >
-                {a}
-              </span>
+            <h4
+              onClick={() => {
+                setCurrentIdx(i);
+                setModal(true);
+              }}
+            >
+              {a}
               <button
                 type="button"
-                onClick={() => {
+                onClick={(e) => {
+                  // 이벤트 버블링 막기
+                  e.stopPropagation();
                   let likeCopy = [...좋아요];
                   likeCopy[i] = likeCopy[i] + 1;
                   좋아요변경(likeCopy);
@@ -51,13 +61,35 @@ function App() {
                 ❤️ {좋아요[i]}
               </button>
             </h4>
-            {/* <h4>{title[i]}</h4> */}
             <p>2월 15일 발행</p>
+            <button
+              type="button"
+              onClick={(e) => {
+                deleteTitle(i);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
 
-      {/* 1. 작명={state이름} */}
+      {/* input 에 뭔가 입력시, 코드 실행하고 싶으면
+      onChange / onInput */}
+      {/* state 변경함수는 늦게 처리됨.(비동기처리) 
+        하단 코드에서 console.log 가 먼저 실행
+        setInputValue 그다음 실행
+      */}
+      <input
+        type="text"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      ></input>
+      <button type="button" onClick={post}>
+        등록
+      </button>
+
       {modal == true ? (
         <Modal
           title={title}
@@ -70,8 +102,6 @@ function App() {
   );
 }
 
-// 함수 바깥에 넣기
-// 영어대문자로 작명시작
 function Modal(props) {
   return (
     <div className="modal">
